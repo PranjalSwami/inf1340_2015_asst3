f7c6256e4ef9905199a643478cda756b792f6e97
PranjalSwami <pranjalsswami@gmail.com>
2015-12-15 17:31:28 -0500

    added code to def valid_passport_format

--- exercise2.py
+++ exercise2.py
@@ -137,7 +137,6 @@ def decide(input_file, countries_file):
                         visa = get_visa(citizen_record)
                         if not is_visa_valid(visa):
                             immigration_statuses.append(IMMIGRATION_REJECT)
-<<<<<<< Updated upstream
                             # If rejected, no need to process further. Continue to next applicant
                             continue
 
@@ -166,7 +165,6 @@ def decide(input_file, countries_file):
 
     return immigration_statuses
 
-<<<<<<< Updated upstream
 def verify_record_complete(citizen_record):
     """
     Checks whether all fields in an entry record are filled in
@@ -185,12 +183,9 @@ def verify_record_complete(citizen_record):
 
     return is_valid_record
 
-=======
-                            
-=======
 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
+
 def valid_passport_format(passport_number):
     """
     Checks whether a pasport number is five sets of five alpha-number characters separated by dashes
@@ -211,6 +206,12 @@ def valid_visa_format(visa_code):
     :return: Boolean; True if the format is valid, False otherwise
 
     """
+
+    match = visa_matcher.match(visa_code)
+    if match is not None and len(match.group()) > 0:
+        return True
+    else:
+        return False
 
 
 def valid_date_format(date_string):

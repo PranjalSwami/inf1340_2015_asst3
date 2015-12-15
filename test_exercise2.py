60ba58b8905f0b4a704c512a65a3d926636aa671
Hirra Sheikh <hirra.sheikh@hotmail.com>
2015-12-15 18:18:21 -0500

    Test case

--- test_exercise2.py
+++ test_exercise2.py
@@ -19,6 +19,26 @@ def test_returning():
     """
     Travellers are returning to KAN.
     """
+
+    # Visitor 1 is from KAN (no visa required) and has no visa - "Accept"
+    # Visitor 2 is from KAN (no visa required) and has no visa - "Accept"
+    # Visitor 3 is from KAN (no visa required) but coming from LUG with medical advisory - "Quarantine"
+
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
+
+def test_visiting():
+    """
+    Travellers are visiting KAN
+    """
+
+    # Visitor 1 is from KRA (visa required) with a valid visa - "Accept"
+    # Visitor 2 is from LUG (visa required) with a valid visa but has a "Mumps" medical advisory - "Quarantine"
+    # Visitor 3 is from HRJ (no visa required) with no visa - "Accept"
+    # Visitor 4 is from HRJ (no visa required) but is coming via LUG which as medical advisory - "Quarantine"
+    # Visitor 5 is from GOR (no visitor visa, but transit visa required) with no visa - "Accept"
+    # Visitor 6 is from FRY (visa required) with no visa - "Reject"
+    # Visitor 7 is from BRD (visa required) with visa
+    assert decide("test_visitor.json", "countries.json") ==\
+           ["Accept", "Quarantine", "Accept", "Quarantine", "Accept", "Reject", "Reject"]
 

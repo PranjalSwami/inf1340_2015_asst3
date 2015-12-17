b397feac8714ed168d9d0d541c189ba00ab72df7
PranjalSwami <pranjalsswami@gmail.com>
2015-12-16 19:51:35 -0500

    added some test cases

--- test_exercise1.py
+++ test_exercise1.py
@@ -5,11 +5,12 @@
 Test module for exercise3.py
 
 """
+import pytest
 
 __author__ = 'Pranjal, Billal, Hirra'
 
 
-from exercise1 import selection, projection, cross_product
+from exercise1 import selection, projection, cross_product, UnknownFunctionException, UnknownAttributeException
 
 
 ###########
@@ -31,6 +32,8 @@ R2 = [["Department", "Head"],
       ["production", "Mori"],
       ["sales", "Brown"]]
 
+R = [["A", "B", "C"], [1, 2, 3], [4, 5, 6]]
+
 
 #####################
 # HELPER FUNCTIONS ##
@@ -46,6 +49,8 @@ def is_equal(t1, t2):
 #####################
 # FILTER FUNCTIONS ##
 #####################
+
+
 def filter_employees(row):
     """
     Check if employee represented by row
@@ -56,8 +61,6 @@ def filter_employees(row):
     :return: True if the row satisfies the condition.
     """
     return row[-2] >= 30 and row[-1] > 3500
-
-
 
 
 def last_elem_gt_three(row):
@@ -72,6 +75,7 @@ def last_elem_gt_three(row):
 ###################
 # TEST FUNCTIONS ##
 ###################
+
 
 def test_selection():
     """
@@ -88,11 +92,16 @@ def test_selection():
     # Test empty table returned when input table is null
     assert is_equal([], selection([], last_elem_gt_three))
 
+    # Test filter employee method selection
     result = [["Surname", "FirstName", "Age", "Salary"],
               ["Verdi", "Nico", 36, 4500],
               ["Smith", "Mark", 40, 3900]]
-
     assert is_equal(result, selection(EMPLOYEES, filter_employees))
+
+    # If selection function is not passed, should raise exception
+    with pytest.raises(UnknownFunctionException):
+        not_a_fn = []
+        selection(R, not_a_fn)
 
 
 def test_projection():
@@ -105,13 +114,25 @@ def test_projection():
               ["Black", "Lucy"],
               ["Verdi", "Nico"],
               ["Smith", "Mark"]]
-
     assert is_equal(result, projection(EMPLOYEES, ["Surname", "FirstName"]))
+
+    result = [["A", "C"], [1, 3], [4, 6]]
+    assert is_equal(result, projection(R, ["A", "C"]))
+
+    # If attributes passed in is empty, return empty table
+    assert is_equal([], projection(EMPLOYEES, []))
+
+    # Empty table as input should return empty table
+    assert is_equal([], projection([], ["Surname", "FirstName"]))
+
+    with pytest.raises(UnknownAttributeException):
+        projection(EMPLOYEES, ["InvalidColumn"])
+
+        # Mix of valid and invalid columns should still raise exception
+        projection(EMPLOYEES, ["Surname", "InvalidColumn"])
 
 
 def test_cross_product():
-    result = [["A", "C"], [1, 3], [4, 6]]
-    assert is_equal(result, projection(R, ["A", "C"]))
 
     result = [["Employee", "Department", "Department", "Head"],
               ["Smith", "sales", "production", "Mori"],
@@ -120,5 +141,26 @@ def test_cross_product():
               ["Black", "production", "sales", "Brown"],
               ["White", "production", "production", "Mori"],
               ["White", "production", "sales", "Brown"]]
-
     assert is_equal(result, cross_product(R1, R2))
+
+    # Empty tables should give back original table
+    assert is_equal(R1, cross_product(R1, []))
+    assert is_equal(R2, cross_product([], R2))
+
+    # If both tables empty, return
+    assert cross_product([], []) is None
+
+    # Test case with different schema
+    table1 = [["First Name"],
+              ["bat"],
+              ["super"]]
+    table2 = [["Last Name"],
+              ["man"],
+              ["girl"]]
+
+    result = [["First Name", "Last Name"],
+              ["bat", "man"],
+              ["bat", "girl"],
+              ["super", "man"],
+              ["super", "girl"]]
+    assert is_equal(result, cross_product(table1, table2))
